7c5cce0530e48e6734340a3e99021c2733da291b
Pranoy Ray <80657118+pranoy-ray@users.noreply.github.com>
2025-04-21 17:37:31 -0400

    Add files via upload

--- cubicTI.py
+++ cubicTI.py
@@ -6,7 +6,9 @@ a) plotvista() : Plots any 3D numpy array as a cube, with a colorbar/heatmap
 b) slicedplot() : Plots the 3D numpy array's 3 orthogonal planes that slice it through it's center, with a colorbar/heatmap
 c) dualplot() : Plots the above described heatmaps on the same plot as a single image, with a colorbar/heatmap
 
-If you use this code, please cite: https://doi.org/10.1007/s40192-024-00389-9
+If you use this code, please cite:
+(i) https://doi.org/10.1007/s40192-024-00389-9
+(ii) https://doi.org/10.5281/zenodo.15258119
 
 Mathematical Details of Trigonometric Interpolation: https://en.wikipedia.org/wiki/Trigonometric_interpolation
 Implementation Example: https://doi.org/10.1007/s40192-024-00389-9
